72327ac93b7afc7621f0c4648190fb31fad3e0d8
OstPerino <kucenko.nikita@gmail.com>
2022-06-23 17:05:20 +0700

    Add modules for users and posts

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaService } from "./prisma/prisma.service";
+import { PostModule } from './post/post.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, PrismaService, PostModule, UserModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ server/src/user/dto/create-user.dto.ts
@@ -0,0 +1,5 @@
+export class CreateUserDto {
+    id: Number
+    email: String
+    name: String
+}
