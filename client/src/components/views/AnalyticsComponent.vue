c7cac3b6c1e6cdf75f039dd01300e0ba1d62d874
OstPerino <kucenko.nikita@gmail.com>
2022-07-14 19:54:30 +0700

    Solve problems

--- client/src/components/views/AnalyticsComponent.vue
+++ client/src/components/views/AnalyticsComponent.vue
@@ -1,7 +1,7 @@
 <template>
   <div class='AnalyticsComponent'>
     <form class='AnalyticsComponent__chooseInterval chooseInterval'>
-      <ArrowLeft class='leftArrow'/>
+      <ArrowLeft class='leftArrow' />
       <select class='chooseInterval__select select'>
         <option value='day' class='select__item'>Day</option>
         <option value='week' class='select__item'>Week</option>
@@ -11,17 +11,7 @@
         @click.prevent=''
         button-content='Check analytics'
       />
-      <ArrowRight class='rightArrow'/>
-    <form class='AnalyticsComponent__chooseInterval'>
-      <v-btn
-        class="mx-2"
-        fab
-        dark
-        large
-        color="cyan"
-      >
-        asd
-      </v-btn>
+      <ArrowRight class='rightArrow' />
     </form>
     <div class='AnalyticsComponent__tasks tasks'>
       <div class='tasks__header'>
@@ -60,6 +50,7 @@ export default {
 .AnalyticsComponent {
   width: 1300px;
   margin: 0 auto;
+
   &__chooseInterval {
     padding: 10px;
     border-radius: 16px;
@@ -71,6 +62,7 @@ export default {
     justify-content: center;
     align-items: center;
     margin-bottom: 30px;
+
     &__select {
       font-size: 20px;
       line-height: 20px;
@@ -80,17 +72,21 @@ export default {
       color: #fff;
       margin-right: 40px;
     }
+
     .rightArrow {
       margin-left: 40px;
     }
+
     .leftArrow {
       margin-right: 45px;
     }
   }
+
   .tasks {
     padding: 30px;
     background-color: #F9F9F9;
     border-radius: 16px;
+
     &__header {
       background-color: #fff;
       margin-bottom: 10px;
@@ -98,7 +94,6 @@ export default {
       border-radius: 16px;
       text-align: center;
     }
-  &__chooseInterval {
   }
 }
 </style>
